2c197e005c47968b1ea95f5eeed1cdc340c350cb
deependra-bnma <deependra@bnma.us>
2025-05-22 10:24:59 +0530

    Live build upload

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,5 +1,5 @@
 // config.js
-export const DOMAIN_NAME = 'https://factory.demosite.name';
+export const DOMAIN_NAME = 'https://digifactory.ae';
 //https://factory.demosite.name
 //https://digifactory.ae
 export const API_URL = `${DOMAIN_NAME}/api`;
